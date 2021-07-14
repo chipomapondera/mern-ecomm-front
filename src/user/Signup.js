92e45161211995cf8f21f300293b2918cdfd06ae
user <user@users-MacBook-Pro.local>
2021-07-14 11:46:52 +0200

    frontend pages home, signin, signup w menu, layout

---
+++ src/user/Signup.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Layout from "../core/Layout";
+import { API } from "../config";
+
+const Signup = () => {
+    
+    return (
+        <Layout 
+            title="Signup" 
+            description="Signup to Node React E-Comm App"
+            className="container col-md-8 offset-md-2"
+        >
+            ...
+        </Layout>
+    );
+};
+
+export default Signup;
