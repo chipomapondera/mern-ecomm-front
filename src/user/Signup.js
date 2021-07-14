4dea89847ec0ff5b192e7c3f17b0a4bc6de91965
user <user@users-MacBook-Pro.local>
2021-07-14 12:42:57 +0200

    refactored signup, auth index

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { Link } from "react-router-dom";
-import { API } from "../config";
+import { signup } from "../auth"
 
 const Signup = () => {
 
@@ -18,24 +18,6 @@ const Signup = () => {
     const handleChange = name => event => {
         setValues({...values, error: false, [name]: event.target.value});
     };
-
-    const signup = user => {
-        // console.log(name, email, password);
-        return fetch(`${API}/signup`, {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(user)
-        })
-        .then(res => {
-            return res.json();
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }
 
     const clickSubmit = event => {
         event.preventDefault();
