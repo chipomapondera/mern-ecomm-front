74caf3407b1c32de8ea63595e786ab73cfad281f
user <user@users-MacBook-Pro.local>
2021-07-16 11:02:38 +0200

    added dashboard authentication logic

---
+++ src/user/UserDashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+
+const Dashboard = () => {
+
+    return (
+        <Layout title="Dashboard" description="User Dashboard" className="container">
+            ...
+        </Layout>
+    );
+};
+
+export default Dashboard;
