c75ce4aa63ca431f002d222e0618e6e0f58a8ffb
user <user@users-MacBook-Pro.local>
2021-07-27 13:14:36 +0200

    displaying products by sold, arrival on home page

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
 
 const Home = () => {
+    const [productsByArrival, setProductsByArrival] = useState([]);
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [error, setError ] = useState(false);
+
+    const loadProductsByArrival = () => {
+        getProducts("createdAt")
+        .then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                setProductsByArrival(data)
+            }
+        });
+    };
+
+    const loadProductsBySell = () => {
+        getProducts("sold")
+        .then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                setProductsBySell(data)
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, [])
+
     return (
         <Layout 
             title="Home Page" 
             description="Node React E-Comm App"
         >
-            ...
+            {JSON.stringify(productsByArrival)}
+            <hr/>
+            {JSON.stringify(productsBySell)}
         </Layout>
     );
 };
