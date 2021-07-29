8fe7b4c97b7e9c38acab98528c3bcf30bd715e20
chipomapondera <chipomapondera@gmail.com>
2021-07-29 13:46:17 +0200

    testing ssh

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { getCategories } from "./apiCore";
 
 const Checkbox = ({ categories }) => {
     const [checked, setChecked] = useState([]);
