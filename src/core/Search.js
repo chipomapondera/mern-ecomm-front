70aa5ac4954c8e2b0ec515193448387a433bb9bd
chipomapondera <chipomapondera@gmail.com>
2021-08-03 16:12:22 +0200

    add search bar w dropdown and input to home page

--- src/core/Search.js
+++ src/core/Search.js
@@ -33,8 +33,58 @@ const Search = () => {
         loadCategories();
     }, [])
 
+    const searchSubmit = () => {
+        //
+    };
+
+    const handleChange = () => {
+
+    };
+
+    const searchForm = () => {
+        return (
+            <form onSubmit={searchSubmit}>
+                <span className="input-group-text">
+                    <div className="input-group input-group-lg">
+                        <div className="input-group-prepend">
+                            <select 
+                                className="btn mr-2" 
+                                onChange={handleChange("category")}
+                            >
+                                <option value="All">Select category</option>
+                                {categories.map((category, index) => (
+                                    <option key={index} value={category.index}>
+                                        {category.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <input 
+                            type="search" 
+                            className="form-control"
+                            onChange={handleChange("search")} 
+                            placeholder="Search by name"
+                        />
+                    </div>
+                    <div 
+                        className="btn input-group-append" 
+                        style={{brder:"none"}}
+                    >
+                        <button className="input-group-text">
+                            Search
+                        </button>
+                    </div>
+                </span>
+            </form>
+        );
+    };
+
     return (
-        <h2>Search bar {JSON.stringify(categories)}</h2>
+        <div className="row">
+            <div className="container mb-5">
+                {searchForm()}
+            </div>
+        </div>
     );
 };
 
