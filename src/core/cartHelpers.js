1e832feb93d1f49ffc1bb1ce473553f8cf598a08
chipomapondera <chipomapondera@gmail.com>
2021-08-21 16:13:27 +0200

    add prod btn adds prod > localStorage, routes > cart

---
+++ src/core/cartHelpers.js
@@ -0,0 +1,27 @@
+export const addItem = (item, next) => {
+    let cart = [];
+    if (typeof window !== "undefined") {
+        if (localStorage.getItem("cart")) {
+            cart = JSON.parse(localStorage.getItem("cart"));
+        }
+        cart.push({
+            ...item,
+            count: 1
+        });
+
+        // remove duplicates
+        // build an Array from new Set and turn it back into array using Array.from
+        // so that later we can re-map it
+        // so pass the ideas of each object/ product
+        // if the loop tries to add the same value again, it'll get ignored
+        // ...with the array of ids we got when first map() was used
+        // run map() on it again and return the actual product from the cart
+
+        cart = Array.from(new Set(cart.map(product => (product._id)))).map(id => {
+            return cart.find(product => product._id === id);
+        });
+        
+        localStorage.setItem("cart", JSON.stringify(cart));
+        next();
+    }
+}
