75a5baf2fbf51523f9245b8add555a9b5f4d2251
chipomapondera <chipomapondera@gmail.com>
2021-08-22 17:20:12 +0200

    get braintree client token for

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -85,5 +85,22 @@ export const listRelated = productId => {
         })
 };
 
+export const getBraintreeClientToken = (userId, token) => {
+    return fetch(`${API}/braintree/getToken/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+    })
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
+
 
 
