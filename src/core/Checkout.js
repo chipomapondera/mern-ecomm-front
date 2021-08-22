8dcef91255d558994c851d5f57dd5aa1cdc288ce
chipomapondera <chipomapondera@gmail.com>
2021-08-22 17:29:16 +0200

    add checkout dropin payment form to checkout

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -4,6 +4,7 @@ import { getProducts, getBraintreeClientToken } from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
 
 const Checkout = ({ products }) => {
     const [data, setData] = useState({
@@ -40,7 +41,7 @@ const Checkout = ({ products }) => {
 
     const showCheckout = () => {
         return isAuthenticated() ? (
-            <button className="btn btn-success">Checkout</button>
+            <div>{showDropIn()}</div>
         ) : (
             <Link to="signin">
                 <button className="btn btn-primary">
@@ -49,6 +50,19 @@ const Checkout = ({ products }) => {
             </Link>
         )
     };
+
+    const showDropIn = () => (
+        <div>
+            {data.clientToken !== null && products.length > 0 ? (
+                <div>
+                    <DropIn options={{
+                        authorization: data.clientToken
+                    }} onInstance={instance => (data.instance = instance)} />
+                    <button className="btn btn-success">Checkout</button>
+                </div>
+            ) : null}
+        </div>
+    )
 
     return (
         <div>
